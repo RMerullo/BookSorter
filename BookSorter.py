7ade7d91deed0397569ea9fdfc9056c142ffd802
Russell Merullo <rmerullo1@gmail.com>
2025-10-30 10:25:22 -0400

    Sort by title and author

--- BookSorter.py
+++ BookSorter.py
@@ -1,21 +1,38 @@
 from array import array
 
-file_path = "lovahs_books.txt"
+# Books in text file are formatted as:
+# Title | Author
+
+file_path = "books_unsorted.txt"
 book_array = list()
-sorted_by_title = list()
 try:
     with open(file_path, 'r') as file:
-        content = file.read().splitlines()
-        print(f"Content: {content}")
-        book_array.append(content);
+        for line in file:
+            currentLine = line.rstrip()
+            currentLine.replace("\n","")
+            content = currentLine.split('|')
+            # Entry was not formatted correctly
+            if(len(content) != 2):
+               raise ValueError("The following entry is not formatted correctly: \n" + content[0])
+            book_array.append(content)
 except FileNotFoundError:
     print(f"Error: The file '{file_path}' was not found.")
 except Exception as e:
     print(f"An error occurred: {e}")
 
-print(f"Book Array: {book_array}")
-print(f"Sorted Book Array: {book_array.sort}")
+# Sort books by title and write to file
+with open("SortedByTitle.txt", "w") as new_file:
+  for book in sorted(book_array):
+    new_file.write(book[0] + " by " + book[1])
+    new_file.write("\n")
 
-# with open("Sorted_Books.txt", "w") as new_file:
-#   for book in sorted(book_array):
-#     new_file.write(book)
+# Sort books by author and write to file
+books_sorted_by_title = list()
+for book in book_array:
+    books_sorted_by_title.append(book[1] + " | " + book[0])
+with open("SortedByAuthor.txt", "w") as new_file:
+    for book in sorted(books_sorted_by_title):
+        new_file.write(book)
+        new_file.write("\n")
+
+print(f"Success!")
